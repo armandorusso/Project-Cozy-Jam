7b2a1ad7ebe5b23595ed933929cfc4608c1b2b67
George Mavroeidis <gdmavroeidis@hotmail.com>
2023-11-04 02:08:37 -0400

    Create player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private enum PlayerType
+    {
+        Main,
+        Secondary
+    };
+    
+    [SerializeField] private PlayerType _playerType;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _movementSpeed;
+    private Vector2 _inputMovement;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (!_rigidbody2D)
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _inputMovement = context.ReadValue<Vector2>();
+    }
+
+    private void MovePlayer()
+    {
+        if (_playerType == PlayerType.Main)
+        {
+            transform.Translate(_inputMovement * _movementSpeed * Time.deltaTime);
+        }
+    }
+}
